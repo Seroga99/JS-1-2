b810cca68f3cdc94aa5b91e5693a704582cfefa2
Seroga Humenchuk <niva1999niva@gmail.com>
2019-01-30 10:42:28 +0200

    create repo and add module-1

---
+++ module-1/lesson-1/script.js
@@ -0,0 +1,134 @@
+'use strict';
+// const day = 29;
+// const month = 6;
+// const year = 1919;
+// const BIRTHDAY = `I born in ${day}/${month}/${year}`;
+// console.log(BIRTHDAY);
+
+// const SALARY = '22000.40UAH';
+// let parsNumber = Number.parseFloat(SALARY);
+// console.log(parsNumber);
+
+// let bool = confirm('bool');
+// console.log(bool);
+
+
+// const text = 'Индексация символов строки начинается с нуля, не с единицы! Всегда важно об этом помнить. К примеру в строке "JavaScript is amazing!" буква "J" стоит на позиции с индексом 0.';
+// let searchValue = prompt('Enter needed word:');
+// let search = text.indexOf(searchValue);
+// console.log(search);
+
+
+// console.log('I'm happy');
+// console.log('I\'m happy');
+
+
+// const and1 = true && true && false && true;
+// const and2 = true && 'false' && true && 'qwerty';
+// const and4 = 'true' && true && NaN && null && '';
+// const and5 = ' ' && true;
+// const or = false || true; 
+// const oR = false || "" || NaN || undefined || null || 'NaN' || true; 
+// const result = undefined || "string" && 10 || NaN;
+// const Result = 0 || null || -0 && !true;
+// console.log(Result);
+
+
+
+// const Num = Number(prompt());
+
+// if(Num > 10) {
+//     console.log('this mumber is more then 10');
+// } else if(Num === 10 ) {
+//     console.log('this NUm === 10');
+// } else {
+//     console.log('this mumber is < then 10');
+// }
+
+
+// На якому курсі універа ти навчаєшся?
+
+// const FIRST = 1;
+// const SECOND = 2;
+// const THIRD = 3;
+// const FOURTH = 4;
+
+// const INPUT = Number(prompt('На якому курсі універа ти навчаєшся?'));
+
+// if (INPUT === FIRST) {
+//     console.log(`Ти навчаєшся на ${FIRST} курсі`);
+// } else if (INPUT === SECOND) {
+//     console.log(`Ти навчаєшся на ${SECOND} курсі`);
+// } else if (INPUT === THIRD) {
+//     console.log(`Ти навчаєшся на ${THIRD} курсі`);
+// } else if (INPUT === FOURTH) {
+//     console.log(`Ти навчаєшся на ${FOURTH} курсі`);
+// } else {
+//     console.log('Такого курсу не існує');
+// }
+
+
+// const mounth = prompt('Введіть номер місяця: ')
+
+// if (!isNaN(mounth)) {
+//     if (mounth <= 2 || mounth === 12) {
+//         console.log('Зима');
+//     } else if (mounth >= 3 && mounth <= 5) {
+//         console.log('весна');
+//     } else if (mounth >= 6 && mounth <= 8) {
+//         console.log('Літо');
+//     } else {
+//         console.log('це осінь або зима або такого місяця не існує');
+//     }
+// } else {
+//     console.log('введіть число');
+// }
+
+
+// let num = prompt();
+
+// if (num > 10) {
+//     console.log('>10');
+// } else {
+//     console.log('<10');
+// }
+
+// num > 10 ? console.log('>10') : console.log('<10');
+
+
+
+// let num = +prompt();
+
+// let a = 10;
+// let b = 12; 
+
+// switch(num) {
+//     case a:
+//         console.log('it is 10');
+//     break;
+//     case b: 
+//         console.log('it is 12');
+//     break;
+//     default: 
+//         console.log('it is not 10 && is not 12');
+// }
+
+
+// switch (INPUT) {
+//     case FIRST:
+//         console.log(`Ти навчаєшся на ${FIRST} курсі`);
+//         break;
+//     case SECOND: 
+//         console.log(`Ти навчаєшся на ${SECOND} курсі`);
+//         break;
+//     case THIRD:
+//         console.log(`Ти навчаєшся на ${THIRD} курсі`);
+//         break;
+//     case FOURTH: 
+//         console.log(`Ти навчаєшся на ${FOURTH} курсі`);
+//         break;
+//     default:
+//         console.log('Такого курсу не існує');
+// }
+
+
