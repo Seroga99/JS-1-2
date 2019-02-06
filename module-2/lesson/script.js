a1be675d46e48de48cc250da76d2e03d36db7130
Seroga Humenchuk <niva1999niva@gmail.com>
2019-02-06 14:43:04 +0200

    m2

--- module-2/lesson/script.js
+++ module-2/lesson/script.js
@@ -50,5 +50,174 @@
 
 
 
+// let counter = 0;
+
+// while(counter < 10 ) {
+//   console.log(counter);
+//   counter +=1;
+// }
+
+// do {
+//   console.log(counter);
+//   counter +=1;
+// } while(counter < 10 );
 
 
+
+// for(let counter = 0; counter < 10; counter++) {
+//   console.log(counter);
+// }
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+
+// for(let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+
+
+const numbers = [1, 3 ,4, 10, 22, 12, 3, 12];
+const userInput =  prompt('Enter number:');
+let hasNum = false;
+
+for(const num of numbers) {
+  console.log(num);
+}
+
+
+
+
+
+// const numbers = [1, 3 ,4, 10, 22, 12, 3, 12];
+// const newArr = [];
+// const newArr2= [];
+
+// for(let i = 0; i < numbers.length; i++) {
+//   if(numbers[i] % 2 === 0) {
+//     newArr.push(numbers[i]);
+//   } else {
+//     newArr2.push(numbers[i]);
+//   }
+// }
+
+// for(const i of numbers) {
+//   if(i % 2 === 0) {
+//     newArr.push(i);
+//   } else {
+//     newArr2.push(i);
+//   }
+// }
+
+
+/*
+  Попросить пользователя ввести произвольную строку
+  и записать ее в переменную string
+  
+  PS: для перебора массива используте цикл for или for...of
+*/
+
+// let string  = 'asdcds dfdsfds sdfdsf sdfds';
+// let arr;
+
+// Разбить строку в массив, пусть разделителем будет пробел, и записать в переменную arr
+
+// arr = string.split('');
+// console.log(arr);
+
+// Вывести в консоли общую длину массива arr
+// console.log(arr.length);
+
+// Используя цикл, вывести в консоль все индексы массива arr
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(i);
+// }
+
+
+// Используя цикл, вывести в консоль все элементы массива arr
+
+
+// let newArr = [];
+// for(const i of arr) {
+//   newArr.unshift(i)
+// }
+// console.log(newArr);
+
+// Используя цикл, bывести в консоли все пары индекс:значение массива arr
+// let newArr = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(i);
+// }
+
+
+/*
+  Напишите цикл, который предлагает, через prompt, ввести число больше 100. 
+  
+   Если посетитель ввёл другое число или нажал Cancel – попросить ввести ещё раз, 
+   и так далее, пока не введет число больше 100.
+  
+  Предполагается, что посетитель вводит только числа, обрабатывать невалидный ввод 
+  вроде строк 'qweqwe' в этой задаче необязательно.
+  
+  PS: используйте цикл do...while
+*/
+// let task = 110;
+//  while(task > 100 && task !== null) {
+//    task = prompt('>100')
+//  }
+
+
+/*
+  Напишите скрипт, который выводит через console.log все 
+  числа от min до max, с двумя исключениями: 
+    
+    - Для чисел, нацело делящихся на 3, вместо числа выводится строка 'Fizz'
+    
+    - Для чисел, нацело делящихся на 5, но не на 3, вместо числа выводится строка 'Buzz'
+    
+  PS: используйте цикл for
+*/
+
+// const min = 1;
+// const max = 100;
+
+// for(let i = min; i <= max; i++) {
+//   if(i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if(i % 5 === 0 && i % 3 !== 0)  {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+/*
+  Напишите скрипт, который выбирает из массива numbers 
+  все числа, которые больше чем значение переменной num, 
+  записывая эти числа в массив newArray.
+      
+  В результате в массиве newArray будут все подходяшие числа.
+      
+  PS: используйте цикл for или for...of и оператор ветвления if
+*/
+
+// const numbers = [1, 3, 17, 5, 9, 14, 8, 14, 34, 18, 26];
+// const num = 10;
+// const newArray = [];
+
+
+// for(let i of numbers) {
+//   if(i > 10) {
+//     newArray.push(i);
+//   }
+// }
+// console.log(newArray);
+
+// for(let i=0; i < numbers.length; i++) {
+//   if(numbers[i] > 10) {
+//     newArray.push(numbers[i]);
+//   }
+// }
+// console.log(newArray);
