94d55aee84fcd1a490cc00402287b791430a7f7d
Seroga Humenchuk <niva1999niva@gmail.com>
2019-02-10 15:58:23 +0200

    adds

---
+++ module-1/homework1/script.js
@@ -0,0 +1,18 @@
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+const checkLogin = prompt('Please enter login');
+let checkPassword;
+if (checkLogin === null){
+    alert('Refused by user');
+} else if ( checkLogin !== adminLogin){
+    alert('Incorrect login');
+} else if (checkLogin === adminLogin){
+    checkPassword = prompt('Please enter password');
+    if (checkPassword === adminPassword){
+        alert('Welcome');
+    } else if ( checkPassword === null){
+        alert('Refused by user')
+    } else if ( checkPassword !== adminPassword){
+        alert('Incorrect password') 
+    }
+}   
---
+++ module-3/script.js
@@ -0,0 +1,86 @@
+"use strict"
+// =====================================================
+// const fn = function () {
+//     console.log('Hello, I am function');
+// }
+
+
+// ====================================================
+// let firstNum = +prompt('Enter first number:');
+// let SecondNum = +prompt('Enter second number:');
+// console.log(firstNum);
+// console.log(SecondNum);
+
+// const fnAdd = function (num1, num2) {
+//     let res = num1 + num2;
+//     return res;
+// };
+// const fnMult = function (num1, num2) {
+//     let res = num1 * num2;
+//     return res;
+// };
+// const fnSUB = function (num1, num2) {
+//     let res = num1 - num2;
+//     return res;
+// };
+// const fnDiv = function (num1, num2) {
+//     let res = num1 / num2;
+//     return res;
+// };
+// const fnShow = function (add, mult, sub, div) {
+//     console.log('+ ' + add(firstNum, SecondNum));
+//     console.log('* ' + mult(firstNum, SecondNum));
+//     console.log('- ' + sub(firstNum, SecondNum));
+//     console.log('/ ' + div(firstNum, SecondNum));
+// }
+// fnShow(fnAdd, fnMult, fnSUB, fnDiv)
+
+// ==================================================
+// const arr1 = [1, 5, 23, 42, 8, 12];
+// let resultArr1 = [];
+// for (const el of arr1) {
+//     if(el < 10) {
+//         resultArr1.push(el)
+//     }
+// }
+// console.log(resultArr1);
+
+// const arr2 = [10, 3, 233, 34, 12, 2];
+// let resultArr2 = [];
+// for (const el of arr2) {
+//     if (el < 15) {
+//         resultArr2.push(el)
+//     }
+// }
+// console.log(resultArr2);
+
+// =================================================
+// те саме, що і вище, тільки через функцію 
+// const fnFindNum = function (arr, num) {
+//     let resultArr = [];
+//     for (const el of arr) {
+//         if (el < num) {
+//             resultArr.push(el)
+//         }
+//     }
+//     return resultArr;
+// }
+
+// console.log(fnFindNum(arr1, 10));
+// console.log(fnFindNum(arr2, 15));
+
+// параметры по умолчанию !!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
