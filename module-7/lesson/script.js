67604881c2583a506c0c249393caf8ce01c2dcc6
Seroga Humenchuk <niva1999niva@gmail.com>
2019-03-17 15:16:53 +0200

    add 7 mod

---
+++ module-7/lesson/script.js
@@ -0,0 +1,59 @@
+"use strict"
+
+/*
+  Есть список категорий с классом categories (на вкладке HTML).
+  
+  Напишите код, который для каждого элемента li (первая вложенность) 
+  в списке categories выведет в консоль:
+  - Текст непосредственно в нём (название категории)
+  - Количество всех вложенных в него элементов li
+  
+  К примеру:
+    Категория: Животные
+    Количество вложенных li: 4
+*/
+
+
+// let cat = document.querySelector('.item');
+// cat.classList.toggle('red__color');
+// console.log(cat);
+
+
+
+
+
+
+// const img = document.querySelector('.image');
+
+
+
+// img.src = "http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg";
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// const list = document.querySelector('.list');
+
+
+// const newItem = el => `<li>${el}</li>`;
+
+// for(let i=4; i <= 10; i++) {
+//     list.innerHTML += newItem(i);
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let text = document.createElement('p');
+text.textContent = 'I am text'
+let image = document.createElement('img');
+image.setAttribute('src', 'http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg');
+image.setAttribute('alt', 'photo1')
+console.log(image);
+
+
+let body = document.querySelector('body');
+body.prepend(image);
+console.log(body);
