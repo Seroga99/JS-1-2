9ee8f3b30f9f576f8dae1be63258f1a250dd08ec
Seroga Humenchuk <niva1999niva@gmail.com>
2019-04-03 16:59:19 +0300

    add module 9 and hmw-8

--- module-8/homework/script.js
+++ module-8/homework/script.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict';
 /*
   Создайте компонент галлереи изображений следующего вида.
   
@@ -54,14 +54,94 @@
 */
 
 const galleryItems = [
-  { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
-  { preview: 'img/preview-2.jpeg', fullview: 'img/fullview-2.jpeg', alt: "alt text 2" },
-  { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
-  { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
-  { preview: 'img/preview-5.jpeg', fullview: 'img/fullview-5.jpeg', alt: "alt text 5" },
-  { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
+  { preview: './img/s1.jpg', fullview: 'img/L1.jpg', alt: 'alt text 1' },
+  { preview: './img/s2.jpg', fullview: 'img/L2.jpg', alt: 'alt text 2' },
+  { preview: './img/s3.jpg', fullview: 'img/L3.jpg', alt: 'alt text 3' },
+  { preview: './img/s4.jpg', fullview: 'img/L4.jpg', alt: 'alt text 4' },
+  { preview: './img/s5.jpg', fullview: 'img/L5.jpg', alt: 'alt text 5' },
+  { preview: './img/s6.jpg', fullview: 'img/L6.jpg', alt: 'alt text 6' },
+  { preview: './img/s8.jpg', fullview: 'img/L8.jpg', alt: 'alt text 8' },
+  { preview: './img/s1.jpg', fullview: 'img/L1.jpg', alt: 'alt text 1' },
+  { preview: './img/s2.jpg', fullview: 'img/L2.jpg', alt: 'alt text 2' },
+  { preview: './img/s3.jpg', fullview: 'img/L3.jpg', alt: 'alt text 3' },
+  { preview: './img/s4.jpg', fullview: 'img/L4.jpg', alt: 'alt text 4' },
+  { preview: './img/s5.jpg', fullview: 'img/L5.jpg', alt: 'alt text 5' },
+  { preview: './img/s6.jpg', fullview: 'img/L6.jpg', alt: 'alt text 6' },
+  { preview: './img/s8.jpg', fullview: 'img/L8.jpg', alt: 'alt text 8' },
 ];
 
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.js-image-gallery');
+  const fullview = document.createElement('div');
+  fullview.classList.add('fullview');
+  const preview__container = document.querySelector('.preview__container');
+  const preview = document.createElement('ul');
+  preview.classList.add('preview');
+  container.prepend(fullview);
+  preview__container.prepend(preview);
+
+  const fullImg = document.createElement('img');
+  fullImg.setAttribute('src', galleryItems[0].fullview);
+  fullview.appendChild(fullImg);
+
+  const createPreview = arr => {
+    const items = [];
+    arr.map(el => {
+      const item = document.createElement('li');
+      const img = document.createElement('img');
+      img.classList.add('grayscale');
+      img.setAttribute('src', el.preview);
+      img.setAttribute('data-fullview', el.fullview);
+      img.setAttribute('alt', el.alt);
+      item.appendChild(img);
+      items.push(item);
+    });
+    preview.prepend(...items);
+  };
+  createPreview(galleryItems);
+
+  const images = document.querySelectorAll('.preview > img');
+  console.log(images);
+
+  preview.addEventListener('click', e => {
+    if (e.target.nodeName === 'IMG') {
+      fullImg.setAttribute('src', e.target.dataset.fullview);
+      e.target.classList.remove('grayscale');
+    }
+    if (fullImg.getAttribute('alt') !== e.target.getAttribute('alt')) {
+    }
+  });
+
+  const carusel = () => {
+    const leftButton = document.querySelector("[data-action='slideLeft']");
+    const rightButton = document.querySelector("[data-action='slideRight']");
+
+    const card = preview.querySelector('li');
+    const cardWidth = card.offsetWidth;
+    const cardMarginRight = +window.getComputedStyle(card).marginRight.match(/\d+/g)[0];
+    const cardCount = preview.querySelectorAll('li').length;
+
+    let offset = 0;
+    const maxX = -(cardCount * cardWidth);
+    leftButton.addEventListener('click', () => {
+      if (offset !== 0) {
+        offset += cardWidth + cardMarginRight;
+        preview.style.transform = `translateX(${offset}px)`;
+      }
+      if (null) {
+      }
+    });
+    rightButton.addEventListener('click', () => {
+      console.log(offset);
+      console.log(maxX + cardWidth * 6);
+      if (offset > maxX + cardWidth * 6) {
+        offset -= cardWidth + cardMarginRight;
+        preview.style.transform = `translateX(${offset}px)`;
+      }
+    });
+  };
+  carusel();
+});
 
 /*
   ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
@@ -78,10 +158,10 @@ const galleryItems = [
   Тогда создание экземпляра будет выглядеть следующим образом.
 */
 
-new Gallery({
-  items: galleryItems,
-  parentNode: document.querySelector('.image-gallery'),
-  defaultActiveItem: 1
-});
+// new Gallery({
+//   items: galleryItems,
+//   parentNode: document.querySelector('.image-gallery'),
+//   defaultActiveItem: 1,
+// });
 
 /* Далее плагин работает в автономном режиме */
