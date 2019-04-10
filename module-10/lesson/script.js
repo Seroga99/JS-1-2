eb5f206255b0ff65d477fc494160640dc9c4b802
Seroga Humenchuk <niva1999niva@gmail.com>
2019-04-10 20:57:45 +0300

    add smth in module 10

--- module-10/lesson/script.js
+++ module-10/lesson/script.js
@@ -1,32 +1,55 @@
 'use strict';
 
-fetch('https://codepen.io/loomernescent/pen/LRGRaB.js')
-  .then(response => response.json())
-  .then(data => {
-    generateList(data);
-    infoArr = data;
-  })
-  .catch(error => {
-    const err = document.createElement('h2');
-    err.textContent = 'Купляй самсунг' + error;
-    document.body.appendChild(err);
-  });
+// fetch('https://codepen.io/loomernescent/pen/LRGRaB.js')
+//   .then(response => response.json())
+//   .then(data => {
+//     generateList(data);
+//   })
+//   .catch(error => {
+//     const err = document.createElement('h2');
+//     err.textContent = 'Купляй самсунг' + error;
+//     document.body.appendChild(err);
+//   });
 
-function generateList(arr) {
+// function generateList(arr) {
+//   const list = document.createElement('ul');
+//   arr.map(el => {
+//     const item = document.createElement('li');
+//     const qoute = document.createElement('h2');
+//     const image = document.createElement('img');
+//     const author = document.createElement('p');
+
+//     qoute.textContent = el.quote;
+//     author.textContent = el.whom;
+//     image.setAttribute('src', el.imgSrc);
+
+//     item.append(image, qoute, author);
+//     list.append(item);
+//   });
+
+//   return document.body.appendChild(list);
+// }
+
+const generateUI = arr => {
+  console.log(arr);
   const list = document.createElement('ul');
   arr.map(el => {
     const item = document.createElement('li');
-    const qoute = document.createElement('h2');
+
+    const name = document.createElement('h2');
+    name.textContent = el.title;
+
     const image = document.createElement('img');
-    const author = document.createElement('p');
+    image.setAttribute('src', `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`);
 
-    qoute.textContent = el.quote;
-    author.textContent = el.whom;
-    image.setAttribute('src', el.imgSrc);
-
-    item.append(image, qoute, author);
-    list.append(item);
+    item.append(name, image);
+    list.appendChild(item);
+    document.body.appendChild(list);
   });
+};
 
-  return document.body.appendChild(list);
-}
+fetch(
+  'https://api.themoviedb.org/3/movie/popular?api_key=5874acfd11651a28c55771624f7021f4&language=en-US',
+)
+  .then(response => response.json())
+  .then(data => generateUI(data.results));
